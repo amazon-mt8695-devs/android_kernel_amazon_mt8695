5f8f13be40787e6ccd2e0976ddc0c1b9da1638bb
R0rt1z2 <rogerortizleal@gmail.com>
2023-04-30 20:53:50 +0200

    include/: Import Amazon kernel changes
    * From FireTVStick-Gen_3-7.2.7.3-20220207.tar.bz2
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- include/linux/random.h
+++ include/linux/random.h
@@ -8,7 +8,7 @@
 
 #include <linux/list.h>
 #include <linux/once.h>
-
+#include <linux/percpu.h>
 #include <uapi/linux/random.h>
 
 struct random_ready_callback {
@@ -46,6 +46,7 @@ struct rnd_state {
 	__u32 s1, s2, s3, s4;
 };
 
+DECLARE_PER_CPU(struct rnd_state, net_rand_state);
 u32 prandom_u32_state(struct rnd_state *state);
 void prandom_bytes_state(struct rnd_state *state, void *buf, size_t nbytes);
 void prandom_seed_full_state(struct rnd_state __percpu *pcpu_state);
